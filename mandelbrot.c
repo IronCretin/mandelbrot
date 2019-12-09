5c58237276333432135a635cb46d27e60886eee1
Paul Maynard <Paul.Maynard001@umb.edu>
2019-12-09 13:15:16 -0500

    ascii c-based renderer

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,46 +1,57 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // compile with:
 // cc -fPIC -shared -o libbrot.so mandelbrot.c
 
-double mag2(double x, double y) {
-    return x*x + y*y;
+const int MAXIT = 100;
+// const int SIZE = 30;
+
+int check(int maxit, double x0, double y0) {
+    double x1, y1, x, y;
+    // we have to track the x and y components separately here because c sucks
+    // start at zero, after 1 step we hit z so its like starting at z
+    x = 0;
+    y = 0;
+    for(int i = 0; i < maxit; i++) {
+        // x2 = x*x;
+        // y2 = y*y;
+        // no need for square root when we can just square the distance
+        if(x*x + y*y >= 4) { // escaped
+            return i;
+        } else {
+            // x and y of z^2
+            x1 = x*x - y*y;
+            y1 = 2*x*y;
+            
+            // add z0 to z^2
+            x = x1 + x0;
+            y = y1 + y0;
+        }
+    }
+    // in set
+    return -1;
 }
 
-
-void check(int maxit, double xs[], double ys[], int size, int out[]) {
-    int escaped;
-    double x0, y0, x1, y1, x, y;
-    for(int n = 0; n < size; n++) {
-        escaped = 0;
-        x0 = xs[n];
-        y0 = ys[n];
-        // we have to track the x and y components separately here because c sucks
-        // start at zero, after 1 step we hit z so its like starting at z
-        x = 0;
-        y = 0;
-        for(int i = 0; i < maxit; i++) {
-            // x2 = x*x;
-            // y2 = y*y;
-            // no need for square root when we can just square the distance
-            if(x*x + y*y >= 4) { // escaped
-                escaped = 1;
-                out[n] = i;
-                break;
+int main(int argc, char *argv[]) {
+    int SIZE = atoi(argv[1]);
+    int steps;
+    float x, y;
+    // int cells[30][30];
+    printf("\033[0;31m"); 
+    for(int i = 0; i < SIZE/2; i++) {
+        y = 4.0 / (SIZE/2) * -i + 2;
+        for(int j = 0; j < SIZE; j++) {
+            x = 4.0 / (SIZE - 1) * j - 2;
+            // printf("%.2f + %.2fi", x, y);
+            steps = check(MAXIT, x, y);
+            if(steps == -1) {
+                printf("*");
             } else {
-                // x and y of z^2
-                x1 = x*x - y*y;
-                y1 = 2*x*y;
-                
-                // add z0 to z^2
-                x = x1 + x0;
-                y = y1 + y0;
+                printf(" ");
             }
         }
-        // in set
-        if (!escaped){
-            out[n] = -1;
-        }
+    printf("\n");
     }
 }
